0af36e167e0c743e1f6ca6d6594af5b67ebd27dd
vmate0301 <113166715+VhiszkyS@users.noreply.github.com>
2024-10-20 15:18:45 +0200

    Add files via upload

---
+++ IloanService.cs
@@ -0,0 +1,15 @@
+﻿namespace Beadandó
+{
+    public interface IloanService
+    {
+        void Add(Loan loan);
+
+        void Delete(Guid BookId);
+
+        Loan Get(Guid BookID);
+
+        List<Loan> GetAll();
+
+        void Update(Loan newLoan);
+    }
+}
