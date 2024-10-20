9600352921a9f805f6b14c39fcd4807d773b3fec
vmate0301 <113166715+VhiszkyS@users.noreply.github.com>
2024-10-20 14:41:59 +0200

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Beadandó
+{
+    public class Book
+    {
+        public Guid Id {  get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Publisher { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "A dátum nem lehet negatív.")]
+        public DateOnly PublishDate { get; set; }
+    }
+}
---
+++ Readers.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Beadandó
+{
+    public class Readers
+    {
+        public Guid id { get; set; }
+
+        public string name { get; set; }
+
+        public string address { get; set; }
+
+        [Range(typeof(DateTime), "1900-01-01", "2100-12-31", ErrorMessage = "A születési dátum nem lehet kisebb mint 1900.")]
+        public DateOnly BirthDate { get; set; }
+
+
+    }
+}
